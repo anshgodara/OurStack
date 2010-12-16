67044ac6b2e8fc3aedfa1977a6d23cd64fa9dd2e
Pooja <pooja.kuamri@ericsson.com>
2010-12-16 15:50:34 +0530

    pop functionality

--- src/OurQueue.java
+++ src/OurQueue.java
@@ -1,9 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class OurQueue {
+	
+	private List<String> elements = new ArrayList<String>();
+
 
 	public boolean isEmpty() {
 		// TODO Auto-generated method stub
 		return true;
+	}
+	
+	public void push(String element){
+		this.elements.add(element);
+	}
+
+	public String pop() {
+		if(!elements.isEmpty()){
+			String firstelement = elements.get(0);
+			elements.remove(0);
+			return firstelement;
+		} else
+			return null;
 	}
 
 }
--- test/OurQueueTest.java
+++ test/OurQueueTest.java
@@ -2,12 +2,39 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-
-
 public class OurQueueTest {
 	@Test
 	public void isEmpty() throws Exception {
 		OurQueue queue = new OurQueue();
 		assertTrue(queue.isEmpty());
 	}
+
+	@Test
+	public void popFirstPushedElement() throws Exception {
+		OurQueue queue = new OurQueue();
+		queue.push("First");
+		queue.push("Second");
+		queue.push("Third");
+		String firstelement = queue.pop();
+		assertEquals("First", firstelement);
+
+	}
+
+	@Test
+	public void multiplePushAndPop() throws Exception {
+		OurQueue queue = new OurQueue();
+		queue.push("First");
+		queue.push("Second");
+		queue.push("Third");
+		queue.pop();
+		assertEquals("Second", queue.pop());
+		assertEquals("Third", queue.pop());
+	}
+
+	@Test
+	public void popWithoutAnyElementPresent() throws Exception {
+		OurQueue queue = new OurQueue();
+		assertEquals(null, queue.pop());
+	}
+
 }
