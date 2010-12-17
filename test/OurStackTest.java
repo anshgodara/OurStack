1a29821e39be6ff81f791a521c6b9c549539fc42
Ashish Gupta <ashish.b.gupta@ericsson.com>
2010-12-17 10:38:08 +0530

    adding mock jar

--- test/OurStackTest.java
+++ test/OurStackTest.java
@@ -1,6 +1,7 @@
-import static org.junit.Assert.*;
-
-import javax.sound.sampled.DataLine;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
